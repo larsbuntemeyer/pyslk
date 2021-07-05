22a8304f51710842ea17493e0641db3c8423536c
Lars Buntemeyer <lars.buntemeyer@hzg.de>
2021-07-05 21:27:16 +0200

    docs updates

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,9 @@ extensions = [
         "IPython.sphinxext.ipython_console_highlighting"
 ]
 
+
+autosummary_generate=True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
